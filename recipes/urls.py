221da22800b93779dfcf803840ec624442c654b6
Frederico Villar <fredvillar64@gmail.com>
2022-10-13 20:49:58 -0300

    Alterações nas views

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 app_name = 'recipes'
@@ -7,4 +9,10 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.home,name="home"),
     path('recipes/<int:id>/', views.recipe, name="recipe"),
+    path('recipes/category/<int:category_id>/', views.category, name="category"),    
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
