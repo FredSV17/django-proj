0d2504ccd2b9e330bbcf273c104b1f09b8fd3d40
Frederico Villar <fredvillar64@gmail.com>
2022-10-13 21:02:59 -0300

    Alteração pequena na view

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from .models import Recipe
 
 # Create your views here.
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(is_published=True,).order_by('-id'),)
     return render(request,'recipes/pages/home.html', context={
         'recipes': recipes
     })
