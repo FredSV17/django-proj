4339f036b3803006e792be30296be8dffba126d2
Frederico Villar <fredvillar64@gmail.com>
2022-10-24 22:08:12 -0300

    Criação de testes

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,17 +1,29 @@
 from django.contrib.auth.models import User
-from django.test import TestCase
 from django.urls import resolve, reverse
 from recipes import views
 from recipes.models import Category, Recipe
+from recipes.tests.test_recipe_base import TestRecipeBase
 
 
-class RecipeViewsTest(TestCase):
-    def test_create_recipe(self):
-        self.make
+class RecipeViewsTest(TestRecipeBase):
 
+    # TEST HOME PAGE
     def test_recipe_home_views_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
+
+    def test_recipe_home_view_omits_recipe_if_not_published(self):
+        """
+            Test if recipes that are not published don't appear
+        """
+
+        # Create recipe that is not published
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Should not appear
+        self.assertIn('No recipes here', response.content.decode('utf-8'))
 
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
@@ -22,32 +34,31 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_view_loads_recipe(self):
-        category = Category.objects.create(name='Categ')
-        user = User.objects.create_user(
-            first_name='John',
-            last_name='Doe',
-            username="jdoe",
-            password='123456',
-            email='jdoe@test.com')
-        recipe = Recipe.objects.create(
-            category=category,
-            author=user,
-            title='Recipe Title',
-            description='Recipe description',
-            slug='recipe-slug',
-            preparation_time=11,
-            preparation_time_unit='Minutes',
-            servings=2,
-            servings_unit='Servings',
-            preparation_step='Recipe psteps',
-            preparation_step_is_html=False,
-            is_published=True
-        )
-        assert 1 == 1
+        self.make_recipe()
+        response = self.client.get(reverse('recipes:home'))
+        context = response.content.decode('utf-8')
+        recipes = response.context['recipes']
+        self.assertIn('Recipe Title', context)
+        self.assertIn('11 Minutes', context)
+        self.assertIn('2 Servings', context)
+        self.assertEqual(len(recipes), 1)
 
     def test_recipe_home_no_recipes_found(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertIn('No recipes here', response.content.decode('utf-8'))
+
+    def test_recipe_category_view_loads_recipe(self):
+        title = 'Category test - one recipe only'
+
+        # Create recipe for test
+        self.make_recipe(title=title)
+
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1}))
+        context = response.content.decode('utf-8')
+        self.assertIn(title, context)
+
+    # TEST CATEGORY PAGE
 
     def test_recipe_category_views_function_is_correct(self):
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
@@ -58,11 +69,23 @@ class RecipeViewsTest(TestCase):
             reverse('recipes:category', kwargs={'category_id': 1000}))
         self.assertEqual(response.status_code, 404)
 
-    def test_recipe_detail_views_function_is_correct(self):
-        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
-        self.assertIs(view.func, views.recipe)
-
+    # TEST DETAIL PAGE
     def test_recipe_detail_views_returns_status_code_404_NOT_FOUND(self):
         response = self.client.get(
             reverse('recipes:recipe', kwargs={'id': 1000}))
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_detail_views_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
+
+    def test_recipe_detail_view_loads_correct_recipe(self):
+        title = 'Detail test - One recipe only'
+
+        # Create recipe for test
+        self.make_recipe(title=title)
+
+        response = self.client.get(
+            reverse('recipes:recipe', kwargs={'id': 1}))
+        context = response.content.decode('utf-8')
+        self.assertIn(title, context)
